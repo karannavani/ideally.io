74628873c48048e9c2d9b21540c2c455691379c5
Karan Navani <kknavani@gmail.com>
2018-11-04 23:53:24 +0000

    added character count in idea description

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Feed from './components/Feed'; 
-import Header from './components/Header'; 
-import CreateForm from './components/CreateForm'; 
+import Feed from "./components/Feed";
+import Header from "./components/Header";
+import CreateForm from "./components/CreateForm";
 
 class App extends Component {
   render() {
     return (
       <div>
-      <Header/>
-      <Switch>
-          <Route exact path='/' component={Feed} />
-          <Route path='/new' component={CreateForm} />
-      </Switch>
+        <Header />
+        <div className="app-body">
+          <Switch>
+            <Route exact path="/" component={Feed} />
+            <Route path="/new" component={CreateForm} />
+          </Switch>
+        </div>
       </div>
     );
   }
--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -2,26 +2,55 @@ import React from "react";
 // import axios from "axios";
 
 import {
-    Container,
-    // Row,
-    // Col
+  Container
+  // Row,
+  // Col
 } from "reactstrap";
 
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
 export default class CreateForm extends React.Component {
-    render() {
-        return (
-            <Container>
-            <Form>
-                <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-                </FormGroup>
+  state = {
+    maxChars: 140,
+    charsLeft: 140
+  };
 
-                <Button>Submit</Button>
-            </Form>
-            </Container>
-        );
-    }
+  handleWordCount = ({target:{value}}) => {
+    const charCount = value.length;
+    const maxChars = this.state.maxChars;
+    const charsLeft = maxChars - charCount;
+    this.setState({charsLeft});
+  }
+
+  render() {
+    return (
+      <Container>
+        <Form>
+          <FormGroup>
+            <Label for="title">Title</Label>
+            <Input placeholder="Name of your idea/project" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="description">Desribe your idea</Label>
+            <Input
+              type="textarea"
+              name="text"
+              id="exampleText"
+              maxLength={this.state.maxChars}
+              onChange={this.handleWordCount}
+            />
+            <p>{this.state.charsLeft} remaining</p>
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="description">Looking for</Label>
+            <Input placeholder="Front-end developer" />
+          </FormGroup>
+
+          <Button>Submit</Button>
+        </Form>
+      </Container>
+    );
+  }
 }
